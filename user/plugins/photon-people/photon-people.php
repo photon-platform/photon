5a3dde7db5554f3dbf43938cc909db007f6c9219
i-am-phi <phi@i-am-phi.net>
2018-09-07 21:14:22 -0700

    adding plugins

--- user/plugins/photon-people/photon-people.php
+++ user/plugins/photon-people/photon-people.php
@@ -4,6 +4,12 @@ namespace Grav\Plugin;
 use Grav\Common\Plugin;
 use RocketTheme\Toolbox\Event\Event;
 
+use Grav\Common\Grav;
+use Grav\Common\Page\Collection;
+use Grav\Common\Page\Page;
+use Grav\Common\Page\Pages;
+use Grav\Common\Taxonomy;
+
 /**
  * Class PhotonPeoplePlugin
  * @package Grav\Plugin
@@ -18,17 +24,32 @@ class PhotonPeoplePlugin extends Plugin
   public static function getSubscribedEvents()
   {
       return [
-          'onPluginsInitialized' => ['onPluginsInitialized', 0]
+          'onPluginsInitialized' => ['onPluginsInitialized', 0],
+          'onGetPageTemplates' => ['onGetPageTemplates', 0]
       ];
   }
 
   public function onPluginsInitialized()
   {
-      // If in an Admin page.
-          $this->enable([
-              'onGetPageTemplates' => ['onGetPageTemplates', 0]
-          ]);
-          return;
+
+      if ( $this->isAdmin() ) {
+
+        $this->enable([
+          'onAdminSave' => ['onAdminSave', 0], //from events plugin - not sure if necessary
+          // 'onGetPageTemplates' => ['onGetPageTemplates', 0]
+        ]);
+
+        return;
+      }
+
+
+      $this->enable([
+        'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+        'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+      ]);
+
+      return;
+
   }
 
   /**
@@ -41,5 +62,41 @@ class PhotonPeoplePlugin extends Plugin
       $types->scanBlueprints($locator->findResource('plugin://' . $this->name . '/blueprints'));
       $types->scanTemplates($locator->findResource('plugin://' . $this->name . '/templates'));
   }
+
+  /**
+   * Add current directory to twig lookup paths.
+   */
+  public function onTwigTemplatePaths()
+  {
+      $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+  }
+
+
+  public function onTwigSiteVariables()
+	{
+		// setup
+		$page = 			$this->grav['page'];
+		$pages = 			$this->grav['pages'];
+		// $collection = $pages->all()->ofType('event');
+		$twig = 			$this->grav['twig'];
+		$assets = 		$this->grav['assets'];
+
+		// only load the vars if this datatype page
+		if ($page->template() == 'people')
+		{
+
+      // scripts
+      $js = 'plugin://photon-people/assets/people.js';
+      $assets->addJs($js);
+
+      // styles
+      $css = 'plugin://photon-people/assets/people.css';
+      $assets->addCss($css);
+
+		}
+
+
+
+	}
 
 }
